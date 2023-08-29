f3b9c5920f05290a5b328b7ed25233ddfabeb633
Ankit-Kumar4 <113576375+Ankit-Kumar4@users.noreply.github.com>
2023-08-29 09:36:16 +0530

    Add project files.

---
+++ SealdClass/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SealdClass
+{
+    internal  class Vehicle
+
+    {
+        
+      public void StartEngine()
+        {
+            Console.WriteLine("engine stated:");
+        }
+        public void StopEngine()
+        {
+            Console.WriteLine("Engine Stooped");
+        }
+
+    }
+    sealed class Car:Vehicle
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.StartEngine();
+            car.StopEngine();
+        }
+    }
+}
